221a0f63e33cf742bdc8fd1c8da7b3c385b90c14
NiharShah7 <niharshah554@gmail.com>
2023-10-14 19:04:19 +0530

    first password-generator commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PasswordGenrator';
+  constructor(private toastr: ToastrService) {}
+
+  newLength: any;
+  Mypassword = '';
+
+  letters = false;
+  Numbers = false;
+  Characters = false;
+
+  emptyString = '';
+  strength = '';
+
+
+  ChangedLength(value: any) {
+    console.log(value);
+    this.newLength = parseInt(value); //convert string value to number .
+    console.log(typeof this.newLength);
+
+    if (this.newLength >= 12) {
+      this.strength = 'Strong';
+        
+    } else if (this.newLength <= 10) {
+      this.strength = 'weak';
+      
+    }
+    if (this.newLength <= 5) {
+      this.strength = 'very weak';
+    }
+  }
+
+  IncludeLetters() {
+    this.letters = !this.letters;
+    console.log('Value is ' + this.letters);
+  }
+  IncludeNumbers() {
+    this.Numbers = !this.Numbers;
+    console.log('Value is ' + this.Numbers);
+  }
+  IncludeCharacters() {
+    this.Characters = !this.Characters;
+    console.log('Value is ' + this.Characters);
+  }
+
+  genratePassword() {
+    console.log('Button clicked');
+
+  
+
+
+    const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const num = '1234567890';
+    const specialChar = '!@#$%^&*()?/';
+
+    this.emptyString = '';
+
+    if (this.letters) {
+      this.emptyString = this.emptyString + str;
+    }
+
+    if (this.Numbers) {
+      this.emptyString = this.emptyString + num;
+    }
+
+    if (this.Characters) {
+      this.emptyString = this.emptyString + specialChar;
+    }
+
+    let genratedPassword = '';
+    for (let i = 0; i < this.newLength; i++) {
+      const randomNumber = Math.floor(Math.random() * this.emptyString.length); //multiplied by this.emptyString.length so that the Random number genrated will be between the selected length.
+      genratedPassword = genratedPassword + this.emptyString[randomNumber];
+    }
+
+    this.Mypassword = genratedPassword;
+  }
+
+  copy(val: any) {
+    val.select(); //selects the input field
+
+    let copiedTxt = document.execCommand('copy'); //copies the text
+
+    if (copiedTxt) {
+      console.log(copiedTxt);
+      this.toastr.success('Copied to Clipboard !');
+    }
+    val.setSelectionRange(0, 100); //copies the text for a given range
+  }
+
+  //npm install ngx-toastr --save
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot(
+    {
+      positionClass:'toast-top-right',
+      progressBar:true,
+      timeOut:5000,
+      progressAnimation:'decreasing',
+      preventDuplicates:true
+    }
+    ),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
